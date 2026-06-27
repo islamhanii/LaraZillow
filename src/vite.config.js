d5fc912944e7773b5d03aaaf034b74642be447a0
islamhanii <islamhani433@gmail.com>
2026-06-27 04:59:08 +0300

    feat: update environment configuration and Docker setup for user permissions and debugging

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,9 +5,18 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
         vue(),
     ],
-})
+    server: {
+        host: '0.0.0.0', // Crucial for Docker
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true, // Needed if your OS doesn't pass file change events into Docker cleanly
+        },
+    },
+});
