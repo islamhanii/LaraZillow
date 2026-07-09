b94150545f96831190c6b4030bbef84e0faa0423
islamhanii <islamhani433@gmail.com>
2026-07-09 08:30:50 +0300

    feat: create Box component and refactor Index page to use Box for listing layout

--- src/resources/js/Pages/Listing/Index.vue
+++ src/resources/js/Pages/Listing/Index.vue
@@ -1,22 +1,27 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show', { listing: listing.id })">
-                <ListingAddress :listing="listing" />
-            </Link>
-        </div>
-        <div>
-            <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
-        </div>
-        <div>
-            <Link :href="route('listing.destroy', { listing: listing.id })" method="delete" as="button">Delete</Link>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', { listing: listing.id })">
+                    <ListingAddress :listing="listing" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy', { listing: listing.id })" method="delete" as="button">Delete
+                </Link>
+            </div>
+        </Box>
     </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import ListingAddress from '@/Components/Listing/ListingAddress.vue'
+import Box from '@/Components/UI/Box.vue'
+
 defineProps({
     listings: Array
 })
