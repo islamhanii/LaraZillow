3da4693b8398077da6b78fcf847cb4cad97bb0aa
islamhanii <islamhani433@gmail.com>
2026-07-12 00:26:19 +0300

    feat: refactor listing display to use Listing component and integrate monthly payment calculation

--- src/resources/js/Pages/Listing/Index.vue
+++ src/resources/js/Pages/Listing/Index.vue
@@ -1,30 +1,11 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <div>
-                <Link :href="route('listing.show', { listing: listing.id })">
-                    <Price :price="listing.price" class="text-2xl font-bold"/>
-                    <ListingSpace :listing="listing" class="text-lg"/>
-                    <ListingAddress :listing="listing" class="text-gray-500"/>
-                </Link>
-            </div>
-            <div>
-                <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
-            </div>
-            <div>
-                <Link :href="route('listing.destroy', { listing: listing.id })" method="delete" as="button">Delete
-                </Link>
-            </div>
-        </Box>
+        <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
     </div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import ListingAddress from '@/Components/Listing/ListingAddress.vue'
-import ListingSpace from '@/Components/Listing/ListingSpace.vue'
-import Price from '@/Components/Listing/Price.vue'
-import Box from '@/Components/UI/Box.vue'
+import Listing from '@/Components/Listing/Listing.vue'
 
 defineProps({
     listings: Array
