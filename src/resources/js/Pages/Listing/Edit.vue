6f1d2bc31271952b8fa6c409e3cac04776e40e89
islamhanii <islamhani433@gmail.com>
2026-07-14 21:20:41 +0300

    feat: add for attributes to input fields for better accessibility

--- src/resources/js/Pages/Listing/Edit.vue
+++ src/resources/js/Pages/Listing/Edit.vue
@@ -2,51 +2,51 @@
     <form @submit.prevent="createListing">
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-4">
-                <label class="label">Beds</label>
-                <input v-model.number="form.beds" type="number" class="input" />
+                <label for="beds" class="label">Beds</label>
+                <input id="beds" v-model.number="form.beds" type="number" class="input" />
                 <div v-if="form.errors.beds" v-text="form.errors.beds" class="input-error"></div>
             </div>
 
             <div class="col-span-4">
-                <label class="label">Baths</label>
-                <input v-model.number="form.baths" type="number" class="input" />
+                <label for="baths" class="label">Baths</label>
+                <input id="baths" v-model.number="form.baths" type="number" class="input" />
                 <div v-if="form.errors.baths" v-text="form.errors.baths" class="input-error"></div>
             </div>
 
             <div class="col-span-4">
-                <label class="label">Area</label>
-                <input v-model.number="form.area" type="number" class="input" />
+                <label for="area" class="label">Area</label>
+                <input id="area" v-model.number="form.area" type="number" class="input" />
                 <div v-if="form.errors.area" v-text="form.errors.area" class="input-error"></div>
             </div>
 
             <div class="col-span-8">
-                <label class="label">City</label>
-                <input v-model="form.city" type="text" class="input" />
+                <label for="city" class="label">City</label>
+                <input id="city" v-model="form.city" type="text" class="input" />
                 <div v-if="form.errors.city" v-text="form.errors.city" class="input-error"></div>
             </div>
 
             <div class="col-span-4">
-                <label class="label">Post Code</label>
-                <input v-model="form.code" type="text" class="input" />
+                <label for="code" class="label">Post Code</label>
+                <input id="code" v-model="form.code" type="text" class="input" />
                 <div v-if="form.errors.code" v-text="form.errors.code" class="input-error"></div>
             </div>
 
             <div class="col-span-8">
-                <label class="label">Street</label>
-                <input v-model="form.street" type="text" class="input" />
+                <label for="street" class="label">Street</label>
+                <input id="street" v-model="form.street" type="text" class="input" />
                 <div v-if="form.errors.street" v-text="form.errors.street" class="input-error"></div>
             </div>
 
             <div class="col-span-4">
-                <label class="label">Street Nr</label>
-                <input v-model="form.street_nr" type="text" class="input" />
+                <label for="street_nr" class="label">Street Nr</label>
+                <input id="street_nr" v-model="form.street_nr" type="text" class="input" />
                 <div v-if="form.errors.street_nr" v-text="form.errors.street_nr" class="input-error">
                 </div>
             </div>
 
             <div class="col-span-12">
-                <label class="label">Price</label>
-                <input v-model.number="form.price" type="number" class="input" />
+                <label for="price" class="label">Price</label>
+                <input id="price" v-model.number="form.price" type="number" class="input" />
                 <div v-if="form.errors.price" v-text="form.errors.price" class="input-error"></div>
             </div>
 
