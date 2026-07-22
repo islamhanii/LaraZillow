d444196d589e53e1a68dc9ab0a37ba0f68c4d3ab
islamhanii <islamhani433@gmail.com>
2026-07-22 09:35:35 +0300

    feat: refactor Listing model to use attribute fillable property

--- src/app/Models/Listing.php
+++ src/app/Models/Listing.php
@@ -2,21 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+#[Fillable(['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'])]
 class Listing extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'beds',
-        'baths',
-        'area',
-        'city',
-        'code',
-        'street',
-        'street_nr',
-        'price',
-    ];
 }
